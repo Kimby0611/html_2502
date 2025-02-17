05018e759976a533b784db714855db1bee707e2b
Kimby0611 <bycamp0604@naver.com>
2025-02-17 16:48:06 +0900

    css display

---
+++ ex_17/js/index.display.js
@@ -0,0 +1,8 @@
+var d = document;
+function funHidden() {
+    d.getElementById("panel").style.display = "block"; 
+}
+
+function imgNone () {
+    d.getElementById("div8-1").style.display = 'none';
+}
